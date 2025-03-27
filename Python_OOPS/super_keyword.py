d4c5ae6a58344892bc916418bb132398ff5ba36e
Guna Palani <gupalani@freshworks.com>
2025-03-27 11:06:49 +0530

    learnt super keyword

--- Python_OOPS/super_keyword.py
+++ Python_OOPS/super_keyword.py
@@ -1,7 +1,23 @@
-class Rectangle :
-      pass
+class Rectangle():
+    def __init__(self,length,width,height):
+        self.length=length
+        self.width=width
+        self.height=height
 
-class square(Rectangle):
-      def __init__(self,length,width):
-            self.length=length
-            self.width=width
+class Square(Rectangle):
+    def __init__(self,length,width,height=None):
+        super().__init__(length,width,height)
+    def area(self):
+        return self.length * self.width
+    
+class Cube(Rectangle):
+    def __init__(self,length,width,height):
+        super().__init__(length,width,height)
+
+    def volume(self):
+        return self.length *self.width * self.height
+
+square = Square(3,3)
+cube = Cube(3,3,3)
+print(square.area())
+print(cube.volume())
